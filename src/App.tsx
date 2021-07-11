803ee7ae3e95d62b2d8de21647da402a9cb76a97
thisKK <sonicrat12@gmail.com>
2021-07-11 20:52:08 +0700

    add structure html

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>
+          <th className="first-col">
+            <input></input>
+          </th>
+          <th className="middle-col">
+            <select name="selecter" id="selecter">
+              <option value="isPrime">isPrime</option>
+              <option value="isFibanacci">isFibanacci</option>
+            </select>
+          </th>
+          <th className="last-col">True</th>
+        </tr>
+      </table>
     </div>
   );
 }
---
+++ src/components/Page.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Page extends Component {
+  render() {
+    return <div>sdadas</div>;
+  }
+}
+
+export default Page;
